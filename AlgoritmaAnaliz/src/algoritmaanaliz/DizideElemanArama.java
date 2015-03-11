7ed14047146d33049568cf864d074c826ebc78d6
zeki tombulu <zekitombulu@gmail.com>
2015-03-11 16:55:48 +0200

    dizide istenilen elemanı bulma
    kullanıcıdan alınan dizi boyutu,buyukKucuk degeri ve index ile aranan
    elaman bulunur

--- AlgoritmaAnaliz/src/algoritmaanaliz/DizideElemanArama.java
+++ AlgoritmaAnaliz/src/algoritmaanaliz/DizideElemanArama.java
@@ -16,29 +16,60 @@ public class DizideElemanArama {
 
     public static void main(String[] args) {
 
-        try {
-            Random rand = new Random();//Random fonsiyon tanımlandi
-            int dizidekiElemanSayisi = rand.nextInt(20) + 1;//dizideki eleman sayisi belirlendi(1 ile 20 arasinda)
-            int dizi[] = new int[dizidekiElemanSayisi];//dizi oluşturuldu
-            for (int i = 0; i < dizi.length; i++) {
-                dizi[i] = rand.nextInt(50) + 1;//dizi rastgele elemanlarla dolduruldu(1 ile 50 arasinda)
+        ElemanBulma();
 
-            }
-            for (int i = 0; i < dizi.length; i++) {
-                System.out.print(dizi[i] + "-");//dizi ekrana yazdırıldı
-            }
-            System.out.println("");
-            ElemanBulma(dizi);
-        } catch (Exception e) {
-            System.out.println("dizide aranan indekste eleman bulunamadi");
-        }
     }
 
-    public static void ElemanBulma(int A[]) {
+    public static void ElemanBulma() {
 
-        System.out.println("bulmak istediğiniz eleman indeksini giriniz :");
-        Scanner aranacakElemanNo = new Scanner(System.in); //Scanner nesnesi ile aranak eleman indexi kalvyeden alindi
-        System.out.println("aranan indeksteki eleman :" + A[aranacakElemanNo.nextInt()]);//aranan elaman ekrana yazıldı
+        System.out.println("dizi boyutunu giriniz");
+        Scanner diziOlusturma = new Scanner(System.in);
+        int boyut = diziOlusturma.nextInt();
+        System.out.println("buyuk için '1' e kucuk için '2' ye basınız");
+        int buyukKucuk = diziOlusturma.nextInt();
+        System.out.println("aranacak indexi giriniz ");
+        int index = diziOlusturma.nextInt();
+
+        Random rand = new Random();//Random fonsiyon tanımlandi
+        int dizi[] = new int[boyut];//dizi oluşturuldu
+        for (int i = 0; i < dizi.length; i++) {
+            dizi[i] = rand.nextInt(50) + 1;//dizi rastgele elemanlarla dolduruldu(1 ile 50 arasinda)
+
+        }
+        System.out.println("sirasiz dizi");
+        for (int i = 0; i < dizi.length; i++) {
+            System.out.print("-" + dizi[i]);;
+
+        }
+        System.out.println("");
+        int temp;   // Yer değiştirmede kullanılacak geçici değişken
+        for (int i = 1; i < dizi.length; i++) {
+            for (int j = 0; j < dizi.length - i; j++) {
+
+                if (dizi[j] > dizi[j + 1]) {
+                    temp = dizi[j];
+                    dizi[j] = dizi[j + 1];
+                    dizi[j + 1] = temp;
+
+                }//Önce gelen elaman bir sonrakinden büyükse ikisi yer değiştiriyor
+            }
+            // Dizinin ardışık elamanlarını karşılaştırmak için kullandığımız döngü
+        }
+        System.out.println("sirali dizi");
+        for (int i = 0; i < dizi.length; i++) {
+            System.out.print("-" + dizi[i]);;
+
+        }
+        System.out.println("");
+
+        if (buyukKucuk == 1) {
+            System.out.println("dizinin en büyük  " + index + " inci elemanı " + dizi[dizi.length - index]);
+
+        } else if (buyukKucuk == 2) {
+            System.out.println("dizinin en kucuk  " + index + " inci elemanı " + dizi[index]);
+        } else {
+            System.out.println("yanlis deger girdiniz");
+        }
 
     }
 }
