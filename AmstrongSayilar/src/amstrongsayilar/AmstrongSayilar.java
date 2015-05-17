c0e4aca2002f7be3d16f3e54b5c97af581cd3f1a
zekiTombulu <zekitombulu@gmail.com>
2015-05-17 21:47:29 +0300

    18 mayıs odevler

---
+++ AmstrongSayilar/src/amstrongsayilar/AmstrongSayilar.java
@@ -0,0 +1,33 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amstrongsayilar;
+
+/**
+ *
+ * @author zeki
+ */
+public class AmstrongSayilar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("100 ile 1000 arasındaki Amstrong sayılar : ");
+        int a,b,c,d;
+        for (int i = 100; i <= 999; i++) {
+             a = i / 100;
+             b = (i - a * 100) / 10;
+             c = (i - a * 100 - b * 10);
+
+             d = (a * a * a) + (b * b * b) + (c * c * c);
+
+            if (i == d) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
---
+++ LineerRecursiveSearch/src/lineerrecursivesearch/LineerRecursiveSearch.java
@@ -0,0 +1,60 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lineerrecursivesearch;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author zeki
+ */
+public class LineerRecursiveSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int uzunluk = 0;
+        System.out.println("Dizinin uzunluğunu giriniz :");
+        Scanner girilenSayi = new Scanner(System.in);      
+        uzunluk = girilenSayi.nextInt();    
+        int dizi[] = new int[uzunluk];    
+        for (int i = 0; i < dizi.length; i++) {    
+            System.out.println((i + 1) + ". elamanı giriniz");
+            dizi[i] = girilenSayi.nextInt();
+        }
+        int arananSayi = 0;
+        System.out.println("aramak istediğiniz sayıyı giriniz :");
+        arananSayi = girilenSayi.nextInt();
+        lineerSearch(dizi, 0, dizi.length, arananSayi);
+
+    }
+
+    public static void lineerSearch(int[] arr, int fIndex, int lIndex, int searchNum) {
+
+        if (fIndex == lIndex) {
+
+            System.out.println("Aranan sayı bulunumadı !");
+
+        } else {
+
+            if (arr[fIndex] == searchNum) {
+                System.out.println("aranan sayı bulundu");
+                System.out.println("aranan sayı =" + searchNum);
+                System.out.println("sayının bulunduğu sıra = " + (fIndex+1));
+
+            } else {
+
+                lineerSearch(arr, fIndex + 1, lIndex, searchNum);
+
+            }
+
+        }
+
+    }
+
+}
